fef7f8b3d206388004bcd2a94ce9e08b2c1116fd
KumudRanjan <98683041+Kumud-22@users.noreply.github.com>
2024-08-05 07:36:38 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+//variables can be form in different patterns
+//const accountID or const account_ID or accountId just remember it should be remember easily
+// javascript have some concept keywords which do action according to commands that can be done by v8 enjine or node.js
+// like if you get var, let or const then assign it to memory..... and console.log uses for print output...
+
+//const means constant value and it wont changes  vs Var means value can be changes
+const accountId = 12345
+//accountId = 3 // not allowed to change cause we use ...const...
+let accountEmail = "kumud@gmail.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+console.log(accountId);
+
