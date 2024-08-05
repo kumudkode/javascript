400cd2b945cc9eee1e595d53dd6557f8842b7d0b
KumudRanjan <98683041+Kumud-22@users.noreply.github.com>
2024-08-05 08:23:01 +0000

    variable undersanding

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -6,9 +6,56 @@
 //const means constant value and it wont changes  vs Var means value can be changes
 const accountId = 12345
 //accountId = 3 // not allowed to change cause we use ...const...
-let accountEmail = "kumud@gmail.com"
-var accountPassword = "1234"
-accountCity = "Delhi"
 
-console.log(accountId);
+let accountEmail = "kumud@gmail.com"
+accountEmail = "raj@gmail.com"// allowed and can be changes
+
+var accountPassword = "1234"
+accountPassword = "12"//allowed to changes
+
+
+accountCity = "Delhi"
+accountCity = "mumbai"
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+//or we can write like this
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+// its easy and change into tabular format
+
+//-------------------------------------------------------------------------//
+// js developer doesnt know that what will be the scope of javascript { var }
+// curly braces are scope and cause of this we were using "var" and it changes all variables 
+// so for fixing this developer introduce "let" that same work like var but only changes where it need to changes instead of all variables
+// so if you have to change variable then use the "let" instead of var
+//---------------------------------------------------------------------------//
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+/*
+Variables can be define in 3ways in javascript "name" as:
+
+1. let name = "karan"
+2. var name = "karan"
+3. name = "karan"
+you can use one at a time
+----------console.log(name);---------------
+but in current time only "let" use for defining variable🫡
+*/
+//============================================================================//
+/*
+ and if you dont define variable it is "undefined" in js example:
+ */
+let name;
+name = "kumud";
+console.log(name);
+
+
+
+
 
