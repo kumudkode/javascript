83f0897b1bfd581da6c2ca7ae4673bddbe2c373b
KumudRanjan <98683041+thekumudranjan@users.noreply.github.com>
2024-08-30 13:32:28 +0000

    conversion of data types

--- 01_basics/03_ConversionOpertions.js
+++ 01_basics/03_ConversionOpertions.js
@@ -19,9 +19,20 @@
 //console.log(valueInNumber)
 
 //--------------------------------------------------------------------------------------------------------------
+// true = 1 , false = 0
+// ' ' = false, " Hello " = true
+
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(booleanIsLoggedIn);
+//---------------------------------------------------------------------------
+
+let someNumber = 55
+let stringNumber = String ( someNumber )
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
 
